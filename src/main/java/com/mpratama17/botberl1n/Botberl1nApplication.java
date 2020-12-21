86beb00b00e002939ad847133a1d85717c6231d5
mpratama17 <yogmhmmd17@gmail.com>
2020-12-21 10:56:23 -1200

    menambahkan config,controller,events model

---
+++ src/main/java/com/mpratama17/botberl1n/Botberl1nApplication.java
@@ -0,0 +1,13 @@
+package com.mpratama17.botberl1n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Botberl1nApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Botberl1nApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpratama17/botberl1n/Botberl1nApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpratama17.botberl1n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Botberl1nApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
